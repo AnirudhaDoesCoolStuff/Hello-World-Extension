99d351a83088f6a4119cf04c166065bd185e7c1b
AnirudhaDoesCoolStuff <anirudha.hero@gmail.com>
2025-06-13 23:07:49 +0530

    Completed First Hello World Chrome Extension

---
+++ popup.js
@@ -0,0 +1,5 @@
+document.getElementById('greetBtn').addEventListener('click', function () {
+  const msg = document.getElementById('message');
+  msg.textContent = '🚀 Extension loaded successfully!';
+  msg.style.display = 'block';
+});
